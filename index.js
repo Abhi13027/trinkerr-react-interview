b42a16b8fb5f23310567c495be5346c406641568
trinkerr-dev <dev@trinkerr.com>
2022-01-09 19:49:40 +0530

    Assignment for Interview

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const data = require('./data');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+const token = process.env.userToken;
+
+app.get('/api/user-access-token', (req, res) => {
+    res.send({token});
+})
+
+
+app.get('/api/data', leanAuth, (req, res) => {
+    res.send(data);
+})
+
+app.post('/api/data', leanAuth, (req, res) => {
+    const {name, ltp, lcp} = req.body;
+    if(!name || !ltp || !lcp){
+        return res.send('Please Provide Valid Data');
+    }
+    if(typeof name !== "string" || typeof ltp !== "number" || typeof lcp !== "number"){
+        return res.send("Please provide the correct data type")
+    }
+    if(typeof name.split("::")[0] !== "string" || name.split("::")[1] !== "NSE"){
+        return res.send("Please check the input format for stock");
+    }
+
+    const newStock = [];
+    newStock.push(name, ltp, lcp);
+    res.send("Added the Stock Successfully");
+})
+
+
+app.listen(5000, () => {
+    console.log('The server is up and running')
+})
+
+
+function leanAuth (req, res, next) {
+    const userAccessToken = req.headers['user-access-token'];
+    if(userAccessToken == token){
+        next();
+    }else{
+        return res.send('The user is unauthorized to have access to the data');
+    }
+}
